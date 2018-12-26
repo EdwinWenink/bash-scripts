1f9f746a4db51bdde58662cb7f882adcd9d52b5f
EdwinWenink <edwinwenink@hotmail.com>
2018-12-26 12:25:39 +0100

    fix mistake in done.sh script

--- done.sh
+++ done.sh
@@ -3,9 +3,9 @@
 # Also: make it accept ranges (use sed for this). E.g. done 1-4.
 
 if [[ $1 ]]; then
-	DONE=$(sed "s/$1//g" ~/todo.txt)
+	DONE=$(sed -n "$1p" ~/todo.txt)
 	sed -i "$1d" ~/todo.txt
-	echo "$DONE [$(date +%T)]" > ~/done.txt
+	echo "$DONE [$(date +%T)]" >> ~/done.txt
 else
 	(echo "Tasks completed: " && cat -n ~/done.txt) | cowsay -f tux
 fi
